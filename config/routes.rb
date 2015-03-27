27e32bf4c1a5805719f64608d917d726ba1ce5c3
dstepanenko <dstepanenko@dstepanenko.ru>
2015-03-27 14:15:14 +0300

    Editor design

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'doc' => 'site#doc',  as: :site_doc
+  get 'doc'    => 'site#doc',    as: :site_doc
+  get 'editor' => 'site#editor', as: :site_editor
 
   root 'site#index'
 end
