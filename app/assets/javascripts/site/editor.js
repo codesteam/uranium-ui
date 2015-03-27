40b1a54629065b88eaf3875c9bd447f909be705c
stepozer <stepozer@gmail.com>
2015-03-27 22:56:15 +0300

    Fix JS for editor

--- app/assets/javascripts/site/editor.js
+++ app/assets/javascripts/site/editor.js
@@ -1,7 +1,10 @@
-$(document).ready(function() {
+initEditor = function() {
     var editor = ace.edit("editor");
     editor = ace.edit("editor");
     editor.setTheme("ace/theme/monokai");
     editor.getSession().setMode("ace/mode/yaml");
     editor.setPrintMarginColumn(1000);
-});
+};
+
+$(document).on('page:load', initEditor);
+$(document).ready(initEditor);
