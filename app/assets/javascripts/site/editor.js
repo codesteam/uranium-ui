5cdc6a84a84740396202a2a8f8f3c1f990010ae2
Ryabov Ruslan <diserve.it@gmail.com>
2015-03-27 23:07:57 +0300

    text editor font-size 16px

--- app/assets/javascripts/site/editor.js
+++ app/assets/javascripts/site/editor.js
@@ -1,6 +1,7 @@
 initEditor = function() {
     var editor = ace.edit("editor");
     editor = ace.edit("editor");
+    editor.setFontSize("16px");
     editor.setTheme("ace/theme/monokai");
     editor.getSession().setMode("ace/mode/yaml");
     editor.setPrintMarginColumn(1000);
